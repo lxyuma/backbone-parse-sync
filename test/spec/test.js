e8917ec5c9c82f2e6d75b0d3aef093af0a17f1c2
ryuma.tsukano <lyumalyuma@gmail.com>
2013-12-14 14:40:29 +0900

    add create

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,45 @@
 /*global describe, it */
+
 'use strict';
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+  var TestModel;
+  var TestCollection;
+  var testObject;
 
-            });
-        });
+  describe('CRUD', function(){
+    before(function(){
+      window.parseHeaders = {
+        ApplicationId: "F28PMYTSoJq3qSVDz4McH5SCjFz9MHJdB9yxYQey",
+        RESTAPIKey: "2nwwwGpjez61enwakbI8iCAow2137ZVuCtngQXby"
+      }
     });
+    beforeEach(function(){
+      TestModel = Backbone.Model.extend({
+        useParse: true,
+        urlRoot: "/test"
+      });
+      testObject = new TestModel();
+      TestCollection = Backbone.Collection.extend({
+        useParse: true,
+        url: "/test",
+        model: TestModel
+      });
+    });
+
+    describe('Create', function(){
+      it('should create on parse', function(done){
+        testObject.set({'name': 'test'});
+        testObject.save();
+
+        new TestCollection().fetch({
+          data: $.param({"order": "-updatedAt"}),
+          success: function(collection){
+            window.parseHeaders.a = collection;
+            expect(collection.last().get('name')).to.equal('test');
+            done();
+          }
+        });
+      });
+    });
+  });
 })();
