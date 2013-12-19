bb8abb9192e2af9c0a3db726bcf46a9cccdbfcbe
ryuma.tsukano <lyumalyuma@gmail.com>
2013-12-19 18:57:24 +0900

    update

--- test/spec/test.js
+++ test/spec/test.js
@@ -27,19 +27,45 @@
     });
 
     describe('Create', function(){
+      beforeEach(function(){
+        this.nameValue = 'test' + (+new Date());
+        testObject.set({'name': this.nameValue});
+      });
       it('should create on parse', function(done){
-        testObject.set({'name': 'test'});
-        testObject.save();
+        testObject.once('sync', function(testObject){
+          expect(testObject.isNew()).to.be.false;
+          done();
+        });
+        testObject.save()
 
-        new TestCollection().fetch({
-          data: $.param({"order": "-updatedAt"}),
-          success: function(collection){
-            window.parseHeaders.a = collection;
-            expect(collection.last().get('name')).to.equal('test');
-            done();
-          }
+      });
+
+      describe('UPDATE', function(){
+        beforeEach(function(){
+          testObject.set({'name': "changed"});
+        });
+        it('should update on parse', function(done){
+          testObject.save({}, {
+            success: _.bind(function(testObject){
+              var sameObject = new TestModel({objectId: testObject.id});
+              sameObject.fetch({
+                success: _.bind(function(sameObject){
+                  expect(sameObject.get('name')).to.equal("changed");
+                  done();
+                }, done)}
+              );
+            }, done)
+          });
         });
       });
     });
+
+// LATEST
+//      new TestCollection().fetch({
+//        data: $.param({"order": "-updatedAt"}),
+//        success: onSuccess
+//      });
+
+
   });
 })();
